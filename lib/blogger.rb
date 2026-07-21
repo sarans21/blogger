bdf33e2d9701c7f01b0e5714417b53610d9be21c
Saran Siriphantnon <sarans@hey.com>
2026-07-21 12:34:21 +0700

    Format code

--- lib/blogger.rb
+++ lib/blogger.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-require_relative 'blogger/version'
+require_relative "blogger/version"
 
-require 'active_support/core_ext/hash/keys'
-require 'date'
-require 'fileutils'
-require 'logger'
-require 'redcarpet'
-require 'rouge'
-require 'rouge/plugins/redcarpet'
-require 'tilt'
-require 'yaml'
+require "active_support/core_ext/hash/keys"
+require "date"
+require "fileutils"
+require "logger"
+require "redcarpet"
+require "rouge"
+require "rouge/plugins/redcarpet"
+require "tilt"
+require "yaml"
 
 module Blogger
   class Error < StandardError; end
@@ -28,30 +28,30 @@ module Blogger
       _validate_options!(options)
 
       @contents_dir = options[:contents_dir]
-      @assets_dir   = options[:assets_dir]
-      @layouts_dir  = options[:layouts_dir]
-      @public_dir   = options[:public_dir]
-      @dev          = options[:dev]
+      @assets_dir = options[:assets_dir]
+      @layouts_dir = options[:layouts_dir]
+      @public_dir = options[:public_dir]
+      @dev = options[:dev]
 
-      @scheme = options[:scheme] || 'http'
-      @host   = options[:host]   || 'localhost'
-      @port   = options[:port]   || 8080
+      @scheme = options[:scheme] || "http"
+      @host = options[:host] || "localhost"
+      @port = options[:port] || 8080
 
       @markdown = Redcarpet::Markdown.new(Renderer, autolink: true, fenced_code_blocks: true)
-      @log      = Logger.new(STDOUT)
+      @log = Logger.new(STDOUT)
     end
 
     def build
       # Layouts, Stylesheets, and Assets.
-      assets_dir = File.join(@public_dir, 'assets')
+      assets_dir = File.join(@public_dir, "assets")
       Dir.mkdir(@public_dir) unless File.exist?(@public_dir)
       Dir.mkdir(assets_dir) unless File.exist?(assets_dir)
 
-      @template = Tilt.new(File.join(@layouts_dir, 'index.haml'))
-      Dir[File.join(@layouts_dir, '*')].each do |f|
+      @template = Tilt.new(File.join(@layouts_dir, "index.haml"))
+      Dir[File.join(@layouts_dir, "*")].each do |f|
         FileUtils.cp_r(f, @public_dir) if File.directory?(f)
       end
-      Dir[File.join(@assets_dir, '*')].each do |f|
+      Dir[File.join(@assets_dir, "*")].each do |f|
         FileUtils.cp_r(f, assets_dir)
       end
 
@@ -83,23 +83,23 @@ module Blogger
       if nav.nil?
         @nav = {}
         nav = @nav
-        nav[''] = [] # Root.
+        nav[""] = [] # Root.
       end
 
       # Walk contents to create index/nav.
-      Dir[File.join(src, '*')].each do |f|
+      Dir[File.join(src, "*")].each do |f|
         if File.directory?(f)
           nav[f.delete_prefix(@contents_dir)] = []
           section_dirpath = File.join(@public_dir, File.basename(f))
           _build_index(f, section_dirpath, nav)
         else
-          dst_file = File.join(dst, File.basename(f, '.md')) + '.html'
+          dst_file = File.join(dst, File.basename(f, ".md")) + ".html"
 
           content = File.read(f)
           front_matter, = _extract_front_matter(content)
-          title = front_matter['title']
-          date  = Date.strptime(front_matter['date'], '%Y-%m-%d').strftime('%Y/%m/%d')
-          draft = front_matter['draft']
+          title = front_matter["title"]
+          date = Date.strptime(front_matter["date"], "%Y-%m-%d").strftime("%Y/%m/%d")
+          draft = front_matter["draft"]
 
           # Do not add nav for draft content.
           if !@dev && draft
@@ -107,7 +107,7 @@ module Blogger
             next
           end
 
-          nav[src.delete_prefix(@contents_dir)] << { title: title, link: link(dst_file), date: date }
+          nav[src.delete_prefix(@contents_dir)] << {title: title, link: link(dst_file), date: date}
         end
       end
     end
@@ -118,7 +118,7 @@ module Blogger
       changed_pages = []
 
       # Contents.
-      Dir[File.join(src, '*')].each do |f|
+      Dir[File.join(src, "*")].each do |f|
         if File.directory?(f)
           section_dirpath = File.join(@public_dir, File.basename(f))
 
@@ -127,15 +127,15 @@ module Blogger
 
           changed_pages += _build_contents(f, section_dirpath)
         else
-          dst_file = File.join(dst, File.basename(f, '.md')) + '.html'
+          dst_file = File.join(dst, File.basename(f, ".md")) + ".html"
 
           @log.info "#{f} => #{dst_file}"
 
           content = File.read(f)
           front_matter, body = _extract_front_matter(content)
-          title              = front_matter['title']
-          description        = front_matter['description']
-          og_image           = front_matter['og_image']
+          title = front_matter["title"]
+          description = front_matter["description"]
+          og_image = front_matter["og_image"]
 
           page = {
             title: title,
@@ -143,16 +143,25 @@ module Blogger
             type: src.delete_prefix(@contents_dir)
           }
 
-          context             = Context.new
-          context.page_title  = "Sarans' Blog | #{title}" # TODO: Configurable.
-          context.title       = title
+          context = Context.new
+          context.page_title = "Sarans' Blog | #{title}" # TODO: Configurable.
+          context.title = title
           context.description = description
-          context.og_image    = og_image
-          context.content     = @markdown.render(body)
-          context.date        = Date.strptime(front_matter['date'], '%Y-%m-%d').strftime('%-d %b, %Y')
-          context.articles    = @nav['articles'].sort { |a, b| b[:date] <=> a[:date] }
-          context.link        = ->(path) { link(path) } # TODO: Extract link() to helper.
-          context.dst_file    = dst_file
+          context.og_image = og_image
+          context.content = @markdown.render(body)
+          context.date = Date.strptime(front_matter["date"], "%Y-%m-%d").strftime("%-d %b, %Y")
+          # context.articles    = @nav['articles'].sort { |a, b| b[:date] <=> a[:date] }
+          context.articles = @nav.except("about_me").sort.reverse
+          context.articles.each do |ar|
+            ar.each do |pages|
+              if pages.is_a?(Array)
+                pages.sort! { |a, b| b[:date] <=> a[:date] }
+                pages.reject! { |a| a[:title] == "About Me" }
+              end
+            end
+          end
+          context.link = ->(path) { link(path) } # TODO: Extract link() to helper.
+          context.dst_file = dst_file
 
           File.write(dst_file, @template.render(context))
 
@@ -164,10 +173,10 @@ module Blogger
     end
 
     def _extract_front_matter(content)
-      splits = content.split('---')
+      splits = content.split("---")
 
       front_matter = YAML.load(splits[1])
-      body         = splits[2]
+      body = splits[2]
 
       [front_matter, body]
     end
